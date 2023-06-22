0e4758f19cbe17161439c4e9915b57a684929712
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-06-22 14:59:19 +0700

    void insert

--- CircularQueues_125/CircularQueues_125.cpp
+++ CircularQueues_125/CircularQueues_125.cpp
@@ -4,6 +4,27 @@ using namespace std;
 class queues {
     int front, rear, max = 5;
     int queues_array[5];
+
+public:
+    queues() {
+        front = -1;
+        rear = -1;
+    }
+
+    void insert() {
+        int num;
+        cout << "enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        //cek apakah antrian penuh
+        if ((front == 0 && rear == max - 1) || (front == rear + 1)) {
+            cout << "\nqueue overvlow\n";
+            return;
+        }
+
+        //cek apakah antrian kosong
+    }
 };
 
 int main()
